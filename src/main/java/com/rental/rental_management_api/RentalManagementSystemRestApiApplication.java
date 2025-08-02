f047dc2e01570918465d598d3546c817739abc8a
Mark Jayson <markjaysondc5@gmail.com>
2025-08-02 17:16:40 +0800

    Initial Commit

---
+++ src/main/java/com/rental/rental_management_api/RentalManagementSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.rental.rental_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagementSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalManagementSystemRestApiApplication.class, args);
+	}
+
+}
